20bc62246f16398ce62bc54cca38c2e4840ec6cf
Evan Meltzer <unturnedisthebestever@gmail.com>
2022-08-29 10:02:57 -0400

    Update App.svelte

--- raspipanelsvelte/src/App.svelte
+++ raspipanelsvelte/src/App.svelte
@@ -1,10 +1,15 @@
 <script>
-	export let name;
+	let thing = 1;
+
+	function goto(){
+		thing++;
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main> 
+	<btn on:click={goto}>Hello</btn>
+
+	{thing}
 </main>
 
 <style>
